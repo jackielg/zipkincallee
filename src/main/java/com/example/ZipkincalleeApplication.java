5ba6cb9750e13ee070e9a8f80347dbf33b0bd570
jackie.liu <jackie.lg@hotmail.com>
2016-12-29 11:19:10 +0800

    add called times.

--- src/main/java/com/example/ZipkincalleeApplication.java
+++ src/main/java/com/example/ZipkincalleeApplication.java
@@ -17,6 +17,8 @@ import java.util.logging.Logger;
 public class ZipkincalleeApplication {
 
 	private static final Logger LOG = Logger.getLogger(ZipkincalleeApplication.class.getName());
+	
+	private static int  counter = 0;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZipkincalleeApplication.class, args);
@@ -32,8 +34,9 @@ public class ZipkincalleeApplication {
 
     @RequestMapping("/")
     public String callHome(){
+    	counter = counter + 1;
         LOG.log(Level.INFO, "Bottom will be called in Callee!");
-        return restTemplate.getForObject("http://localhost:8040", String.class)+"|CALLEE";
+        return restTemplate.getForObject("http://localhost:8040", String.class)+". \r\n This is zipkincallee, I have been called " + counter + " times.";
     }
 
     @Bean
